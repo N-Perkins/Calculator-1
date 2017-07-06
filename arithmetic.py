1b5ab24267a73534ada1a23511954a8779ed5011
Hackbright Student <no-reply@hackbrightacademy.com>
2017-07-06 12:13:51 -0700

    Working Calculator

---
+++ arithmetic.py
@@ -0,0 +1,51 @@
+"""Math functions for calculator."""
+
+
+def add(num1, num2):
+    """Return the the_sum of the two input integers."""
+    the_sum = num1 + num2
+    return the_sum 
+
+    #return 10
+
+
+def subtract(num1, num2):
+    """Return the second number subtracted from the first."""
+    the_sum = num1 - num2
+    return the_sum 
+
+
+def multiply(num1, num2):
+    """Multiply the two inputs together."""
+    the_sum = num1 * num2
+    return the_sum 
+
+
+def divide(num1, num2):
+    """Divide the first input by the second, returning a floating point."""
+    the_sum = num1 / num2
+    return float(the_sum)  
+
+
+def square(num1):
+    """Return the square of the input."""
+    the_sum = num1**2
+    return the_sum
+
+
+def cube(num1):
+    """Return the cube of the input."""
+    the_sum = num1**3
+    return the_sum 
+
+
+def power(num1, num2):
+    """Raise num1 to the power of num2 and return the value."""
+    the_sum = num1 ** num2
+    return the_sum 
+
+
+def mod(num1, num2):
+    """Return the remainder of num1 / num2."""
+    the_sum = num1 % num2
+    return the_sum 
